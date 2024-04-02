c592dde5b1761cefdf7d1b32ddacc9f69c18cf66
abusayeid11 <abusayeidsawon@gmail.com>
2024-04-02 16:20:23 +0600

    start/stop and movement

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 import {updateGround, setUpGround} from './ground.js'
+import {updateDino, setUpDino} from './dino.js'
 
 const worldWidth = 100
 const worldHeight = 30
+const speedScaleInc = 0.00001
 
 
 const worldElem = document.querySelector("[data-world]")
+const scoreElem = document.querySelector("[data-score]")
+const startElem = document.querySelector("[data-startScreen]")
 
 setPixelToWorldScale()
 window.addEventListener('resize', setPixelToWorldScale)
+document.addEventListener("keydown", handleStart, {once:true} )
 
-setUpGround()
+
 
 let lastTime 
+let speedScale
+let score
 function update(time){
 
     if(lastTime == null){
@@ -21,13 +28,34 @@ function update(time){
     }
     const delta = time - lastTime
 
-    updateGround(delta, 1)
+    updateGround(delta, speedScale)
+    updateDino(delta, speedScale)
+    updateSpeedScale(delta)
+    updateScore(delta)
 
     lastTime = time
     window.requestAnimationFrame(update)
+    
 }
 
-window.requestAnimationFrame(update)
+function handleStart(){
+    lastTime = null
+    setUpGround()
+    setUpDino()
+    startElem.classList.add("hide")
+    speedScale = 1
+    score = 0
+    window.requestAnimationFrame(update)
+}
+
+function updateSpeedScale(delta){
+  speedScale += delta*speedScaleInc   
+  scoreElem.textContent = Math.floor(score)
+}
+
+function updateScore(delta){
+     score += delta*0.1
+}
 
 function setPixelToWorldScale(){
     let worldToPixelScale 
